6656ebd7a9b0758a40330778430bde612344c653
Taranpreet Singh <ramghrialoveysingh123@gmail.com>
2025-03-07 01:00:39 +0530

    Home page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import About from "@/components/about/About";
 import HeroBanner from "@/components/banner/HeroBanner";
-import Image from "next/image";
 
 export default function Home() {
   return (
